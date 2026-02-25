bef00bca258560f5071a90e92885d72c92ec21b9
Rizwana <199294075+riizwana@users.noreply.github.com>
2026-02-25 20:40:37 +0000

    Restructuring repository

--- CS50 Python Practice/Lecture 0/hello.py
+++
@@ -1,7 +0,0 @@
-def hello(to="world"):
-        print("Hello,", to)
-
-
-hello()
-name = input("What's your name? ")
-hello(name)
--- CS50 Python Practice/Lecture 2/compare.py
+++
@@ -1,8 +0,0 @@
-x = int(input("What is x? "))
-y = int(input("What is y? "))
-
-if x != y:
-    print("x is not equal to y")
-
-else:
-    print("x is equal to y")
